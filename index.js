a38e49f4a31c97638576361e3086eb1e38db31e3
5000vw <isaackiplangat1999@gmail.com>
2023-10-13 09:50:49 +0300

    like function now working

--- index.js
+++ index.js
@@ -12,26 +12,56 @@ const url = `https://thronesapi.com/api/v2/Characters`
       <img id="imageUrl" src=${character.imageUrl} />
       <h2>${character.fullName}</h2>`
       
-      let likeCont = document.createElement("div")
-      likeCont.className="likes-container"
+      let likeContainer = document.createElement("div")
+      likeContainer.className="likes-container"
+      let likeCount = 0
       let likes = document.createElement("h2")
-      likes.textContent= 0;
-      likeCont.appendChild(likes)
+      likes.textContent= likeCount;
+      likeContainer.appendChild(likes)
       let btn = document.createElement("button");
       btn.id= "like-btn"
       btn.textContent = "Like"
       btn.addEventListener("click",()=>{
         btn.style.color="red"
         btn.id="liked"
-        let newlike = likes++
-        likes.textContent=newlike
+        likeCount++
+        likes.textContent=likeCount
         btn.disabled= true;
       })
-      likeCont.appendChild(btn)
-      gotDiv.appendChild(likeCont)
+
+      let commentSection = document.createElement("form")
+      commentSection.id ="comment-section"
+      let commentInput = document.createElement("textarea")
+      commentInput.id="comment-input"
+      let commentSubmit = document.createElement("button")
+      commentSubmit.id = "comment-btn"
+      commentSubmit.type="submit"
+      commentSubmit.textContent="comment"
+      commentSection.appendChild(commentInput)
+      commentSection.appendChild(commentSubmit)
+
+      likeContainer.appendChild(btn)
+      gotDiv.appendChild(likeContainer)
+      gotDiv.appendChild(commentSection)
       characterDiv.appendChild(gotDiv)
     })
       console.log(characters)
+
+      fetch(url)
+      .then((res) => res.json())
+      .then((characters) => {
+        displayCharacters(characters);
+
+        // Search functionality
+        document.getElementById("search").addEventListener("click", () => {
+          const searchInput = document.getElementById("search-bar").value.toLowerCase();
+          const filteredCharacters = characters.filter((character) => {
+            return character.firstName.toLowerCase().includes(searchInput);
+          });
+          displayCharacters(filteredCharacters);
+        });
+      })
+      .catch((err) => console.log(err));
     })
   .catch(err => console.log(err))
 })
