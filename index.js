9686663f6f5ba5abcc7690f2f07a5712ecf6bb5f
5000vw <isaackiplangat1999@gmail.com>
2023-10-13 08:40:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-const url = `https://api.domainsdb.info/v1/domains/search?limit=50,`
+  const characterDiv =  document.querySelector('.character-div')
+const url = `https://thronesapi.com/api/v2/Characters`
   fetch(url)
   .then(res => res.json())
-  .then(domains => {
-    console.log(domains)
-  })
+  .then(characters =>{
+    characters.forEach(character =>{
+
+      let gotDiv = document.createElement("div");
+      gotDiv.id="got-div";
+      gotDiv.innerHTML=`
+      <img src=${character.imageUrl} />
+      <h2>${character.fullName}</h2>`
+      
+
+      let btn = document.createElement("button");
+      btn.textContent = "like"
+      btn.addEventListener("click",likeFunc)
+
+      const likeFunc =()=>{
+        btn.style.color="red"
+      }
+
+      characterDiv.appendChild(gotDiv)
+    })
+      console.log(characters)
+    })
   .catch(err => console.log(err))
 })
