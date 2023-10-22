9eb745322fe495c2dc8447c55ae2b4421ade25f1
5000vw <isaackiplangat1999@gmail.com>
2023-10-22 12:02:36 +0300

    updated search which works now

---
+++ README.md
@@ -0,0 +1,62 @@
+# Game of Thrones Cast Viewer
+
+An interactive website that allows you to explore the cast of Game of Thrones, like your favorite characters, search for specific characters, and leave comments.
+
+## Live Site
+
+You can access the live site by clicking the following link: [Game of Thrones Cast Viewer](https://5000vw.github.io/domainly/)
+
+## Features
+
+- View the cast of Game of Thrones, including character images and names.
+- Like your favorite characters and see the like count.
+- Search for specific characters by name.
+- Leave comments for your favorite characters.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## API Used
+
+The project extracts data from the [Game of Thrones API](https://thronesapi.com/api/v2/Characters).
+
+## Project Setup Instructions
+
+To run this project locally, follow these steps:
+
+1. Clone the repository to your local machine:
+
+      ``` git clone https://github.com/5000vw/domainly.git```
+
+2. navigate to the project directory:
+
+   ```cd domainly```
+
+3. Open the index.html  file in your web browser to access the website.
+
+## Usage
+1. Explore the Game of Thrones cast.
+
+2.  Like your favorite characters by clicking the "Like" button.
+
+3. Search for specific characters using the search bar.
+4. Leave comments for characters by entering text in the comment section and clicking "comment."
+## License
+This project is licensed under the [MIT License](https://mit.com/license). For more information, please refer to the LICENSE file.
+
+## Author
+Author:  isaac kiplangat
+
+GitHub:  https://github.com/5000vw
+## Contributions
+Contributions are welcome! Feel free to open issues and submit pull requests to improve the project.
+
+## Contact
+If you have any questions or need further assistance, please feel free to contact the author:
+
+### Email: icheruiyot823@gmail.com
+
+Enjoy exploring the Game of Thrones cast and have fun using this website!
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", ()=> {
   const characterDiv =  document.querySelector('.character-div')
-const url = `https://thronesapi.com/api/v2/Characters`
-  fetch(url)
-  .then(res => res.json())
-  .then(characters =>{
-    characters.forEach(character =>{
 
+//function for displaying each character
+  function castView(characters){
+    characters.forEach(character =>{
       let gotDiv = document.createElement("div");
       gotDiv.id="got-div";
       gotDiv.innerHTML=`
       <img id="imageUrl" src=${character.imageUrl} />
       <h2>${character.fullName}</h2>`
       
+      // like section
       let likeContainer = document.createElement("div")
       likeContainer.className="likes-container"
       let likeCount = 0
@@ -24,14 +23,16 @@ const url = `https://thronesapi.com/api/v2/Characters`
       let btn = document.createElement("button");
       btn.id= "like-btn"
       btn.textContent = "Like"
-      btn.addEventListener("click",()=>{
+      likeImage.addEventListener("click",()=>{
         btn.style.color="red"
         btn.id="liked"
         likeCount++
         likes.textContent=likeCount
-        btn.disabled= true;
+        likeImage.disabled= true;
       })
 
+
+      //comment section
       let commentSection = document.createElement("form")
       commentSection.id ="comment-section"
       let commentInput = document.createElement("textarea")
@@ -47,6 +48,8 @@ const url = `https://thronesapi.com/api/v2/Characters`
         commentViewer.id ="comments-viewer"
         let comments = document.createElement("h3")
         comments.id="comments"
+
+        // delete comment
         let deleteComment = document.createElement('button')
         deleteComment.textContent="delete"
         deleteComment.id="delete-btn"
@@ -61,34 +64,57 @@ const url = `https://thronesapi.com/api/v2/Characters`
       commentSection.appendChild(commentInput)
       commentSection.appendChild(commentSubmit)
       likeContainer.appendChild(likeImage)
-      likeContainer.appendChild(btn)
+      
       gotDiv.appendChild(likeContainer)
       gotDiv.appendChild(commentSection)
       characterDiv.appendChild(gotDiv)
     })
-      console.log(characters)
+  }
+   //fetch data from api
+const url = `https://thronesapi.com/api/v2/Characters`
+  fetch(url)
+  .then(res => res.json())
+  .then(characters =>{
+    castView(characters)
+      console.log(characters) 
+    })
 
-      fetch(url)
-      .then((res) => res.json())
-      .then((characters) => {
-        console.log(characters);
-  
-        // Search functionality
-        document.getElementById("search").addEventListener("click", (e) => {
+ // Search functionality
+  function searchCast(characters, query) {
+   query = query.toLowerCase();
+   return characters.filter((character) =>
+   character.firstName.toLowerCase().includes(query)
+  );
+  }
+//display searched cas
+  function displayCast(results){
+  characterDiv.innerHTML=""
+    if(results.length === 0){
+      characterDiv.textContent="No matching items found"
+    } else {
+       castView(results)
+    }
+  }
+     
+  const searchInput = document.getElementById("search-bar")
+  document.getElementById("search").addEventListener("click", (e) => {
           e.preventDefault()
-          const searchInput = document.getElementById("search-bar").value.toLowerCase();
-          console.log(searchInput)
-
-          characters.forEach((character) => {
-            const characterName = character.fullName.toLowerCase();
-            if (characterName.includes(searchInput)) {
-              gotDiv.style.display = "block"; 
-            } else {
-              gotDiv.style.display = "none"; 
-            }
-          });
+          fetch(url)
+          .then((res) => res.json())
+          .then((characters) => {
+          // console.log(characters);
+          // console.log(searchInput.value)
+          const query = searchInput.value.trim()
+          const searchResults =
+          searchCast(characters,query)
+          displayCast(searchResults)})
+          // characters.forEach((character) => {
+          //   const characterName = character.fullName.toLowerCase();
+          //   if (characterName.includes(searchInput)) {
+          //     gotDiv.style.display = "block"; 
+          //   } else {
+          //     gotDiv.style.display = "none"; 
+          //   }
+          // });
         });
-      })
-      })
-  .catch(err => console.log(err))
-})
+  })
